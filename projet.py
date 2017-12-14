412695e28e19c2322977b8d1eede6d1221fd0fc9
kowdin <E132896C@INFO2B2pc02.ensinfo.sciences.univ-nantes.prive>
2017-12-14 13:23:12 +0100

    partage

--- projet.py
+++ projet.py
@@ -1,7 +1,14 @@
 import paving
 from matplotlib import pyplot
 from scipy.sparse.csgraph import connected_components
+import numpy
+from scipy.optimize import least_squares
 import robot
+
+pi=3.141592653590
+
+def convert(r):
+	return 180 * (r/3.141592653590)
 
 
 #getting started partie 1
@@ -32,11 +39,6 @@ fig2.show()
 
 #getting started partie 2
 """
-pi=3.141592653590
-
-def convert(r):
-	return 180 * (r/3.141592653590)
-
 r=robot.FiveBars([-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8,17.8],0,1)
 r.pen_up()
 r.go_home()
@@ -57,7 +59,74 @@ print(r.measure_pose())
 r.ax.plot([5],[-20],color='black',marker='*')
 r.ax.plot([5],[20],color='black',marker='*')
 r.pen_down()
-input("Press <ENTER> to continue...")
 """
 
 #calibration
+
+def make_measurements(r2,commands):
+    r2.actuate(commands[0])
+    r2.pen_down()
+    measures=[]
+    print('   Taking measures ...')
+    for q in commands:
+        r2.actuate(q)
+        x = r2.measure_pose()
+        r2.ax.plot([x[0]],[x[1]],color='black',marker='*')
+        measures.append((x,q))
+    r2.pen_up()
+    r2.go_home()
+    return measures
+
+def calibrate(kinematic_functions,nominal_architecture,measures):
+    # error function
+    def errors(a):
+        err=[]
+        for (x,q) in measures:
+            for fi in kinematic_functions(a,x,q):
+                err.append(fi)
+        return err
+    print('   Calibration processing ...')
+    sol = least_squares(errors,nominal_architecture)
+    print('   status : ',sol.message)
+    print('   error : ',sol.cost)
+    print('   result : ',sol.x)
+    return sol.x
+
+def f_5R(architecture,pose,command):
+    [o11,o12,o21,o22,l1,l2,l3,l4] = architecture
+    [x1,x2] = pose
+    [q1,q2] = numpy.radians(command)
+    f1 = numpy.square((o11+numpy.cos(q1)*l1-x1)) + numpy.square((o12+numpy.sin(q1)*l1-x2)) - numpy.square(l2)
+    f2 = numpy.square((o21+numpy.cos(q2)*l4-x1)) + numpy.square((o22+numpy.sin(q2)*l4-x2)) - numpy.square(l3)
+    return [f1,f2]
+
+r=robot.FiveBars([-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8,17.8],0,1)
+nominal_architecture = [-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8,17.8]
+r.pen_up()
+r.go_home()
+
+commands = []
+commands = commands + [[0,q] for q in range(80,220,20)] + [[q,180] for q in range(-40,110,20)]
+commands = commands + [[40,q] for q in range(80,220,20)] + [[q,140] for q in range(-40,110,20)]
+commands = commands + [[q,180-q] for q in range(-40,80,10)]
+
+commands2 = []
+commands2 = commands2 + [[10,q] for q in range(80,220,10)] + [[q,190] for q in range(-40,100,10)]
+commands2 = commands2 + [[q,180+q] for q in range(-40,40,10)]
+
+#calcul de l'architecture reele
+measures = make_measurements(r,commands)
+real_architechture = calibrate(f_5R,[-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8,17.8],measures)
+
+
+#test de la diference avec une pop test
+measures2 = make_measurements(r,commands2)
+print(measures2)
+print([ numpy.max(f_5R(real_architechture, measures2[i], commands2[i])) for i in range(0,36,1)])
+defaut = numpy.max([ numpy.max(f_5R(real_architechture, measures2[i], commands2[i])) for i in range(0,36,1)])
+
+
+#affichage
+print("architechture : ",real_architechture)
+print("erreur max : ", defaut)
+input("Press <ENTER> to continue...")
